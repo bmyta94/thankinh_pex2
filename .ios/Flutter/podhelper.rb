ea5397ec5afd1828c59195c75624b6f9ed87ba70
bmyta94 <bmyta94@gmail.com>
2025-06-07 23:36:31 +0700

    Update podhelper.rb

--- .ios/Flutter/podhelper.rb
+++ .ios/Flutter/podhelper.rb
@@ -6,16 +6,21 @@ def install_all_flutter_pods(flutter_application_path)
 end
 
 def install_flutter_engine_pod
-  engine_dir = File.expand_path('engine', __dir__)
-  pod 'Flutter', :path => engine_dir
+  engine_dir = File.expand_path(File.join(__dir__, 'engine'))
+  if Dir.exist?(engine_dir)
+    Pod::UI.puts "Using custom Flutter engine at #{engine_dir}"
+    pod 'Flutter', :path => engine_dir
+  else
+    pod 'Flutter', :path => File.expand_path(File.join(__dir__, 'flutter'))
+  end
 end
 
 def install_flutter_plugin_pods(flutter_application_path)
-  plugin_pods_file = File.join(flutter_application_path, '.flutter-plugins-dependencies')
+  plugin_pods_file = File.expand_path(File.join(flutter_application_path, '.flutter-plugins-dependencies'))
   return unless File.exist?(plugin_pods_file)
 
   plugin_pods = JSON.parse(File.read(plugin_pods_file))['plugins']['ios']
   plugin_pods.each do |plugin|
-    pod plugin['name'], :path => File.join(flutter_application_path, '.pub-cache', 'hosted', 'pub.dev', plugin['name'])
+    pod plugin['name'], :path => File.join(flutter_application_path, '.pub-cache', 'hosted', 'pub.dev', plugin['name'] + '-' + plugin['version'])
   end
 end
